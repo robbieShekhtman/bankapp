1b17824c3ace3226bfd965b6e0cd14eacf030a18
Robbie Shekhtman <robbieshekhtman@dhcp-vl2041-34839.eduroam.cornell.edu>
2024-11-09 16:11:37 -0500

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local"; 
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
